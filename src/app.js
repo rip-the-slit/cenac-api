9f1bb3bdeb3baaf539f0e3fde13c63fdf5f53e98
rip-the-slit <171191896+rip-the-slit@users.noreply.github.com>
2026-05-28 02:04:44 +0000

    Implement express app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
-import express from "express"
-import db from "./db.js"
+import express from "express";
+import router from "./routes/index.js";
+import open from "open";
+import getRelativeFilePath from "./config/getRelativeFilePath.js";
 
-const app = express()
+const PORT = 3000;
+const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000")
+app.use(express.json());
+app.use("/", express.static(getRelativeFilePath(import.meta.url, "../public")))
+app.get("/", (req, res) => {
+  res.sendFile(getRelativeFilePath(import.meta.url, "../../public/index.html"));
 })
+app.use("/api", router);
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Ruta no encontrada" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ error: "Error interno del servidor" });
+});
+
+app.listen(PORT, console.log)
+open(`http://localhost:${PORT}/`)
+
+export default app;
